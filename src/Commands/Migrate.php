1f39d7727ce3a02799d854ef9f4f720c8ad22bcf
欲饮琵琶码上催 <lujiahao@88.com>
2023-09-26 11:58:08 +0800

    fix:Migrate指令参数如果为数组那么它必须是一个可选值

--- src/Commands/Migrate.php
+++ src/Commands/Migrate.php
@@ -15,7 +15,7 @@ class Migrate extends Command
         $this->setName('idb:migrate')
             ->addOption('database', null, Option::VALUE_OPTIONAL, 'The database connection to use')
             ->addOption('force', null, Option::VALUE_NONE, 'Force the operation to run when in production')
-            ->addOption('path', null, Option::VALUE_IS_ARRAY, 'The path(s) to the migrations files to be executed')
+            ->addOption('path', null, Option::VALUE_OPTIONAL|Option::VALUE_IS_ARRAY, 'The path(s) to the migrations files to be executed')
             ->addOption('realpath', null, Option::VALUE_NONE, 'Indicate any provided migration file paths are pre-resolved absolute paths')
             ->addOption('pretend', null, Option::VALUE_NONE, 'Dump the SQL queries that would be run')
             ->addOption('seed', null, Option::VALUE_NONE, 'Indicates if the seed task should be re-run')
